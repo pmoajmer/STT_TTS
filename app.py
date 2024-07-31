99939ef11aef65aabe4cf8feadfe2e27d3188dbf
aasherkamal216 <146746287+aasherkamal216@users.noreply.github.com>
2024-07-31 22:40:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.set_page_config(page_icon="👽")
 st.title("Speech to Speech :red[Assistant]")
 
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-instruction = "You are a helpful assistant for Q/A tasks. Respond to user's question in just Urdu language. Keep the answer concise and complete."
-model = genai.GenerativeModel('gemini-1.5-flash', generation_config={"temperature": 0.2, "max_output_tokens":100},
+instruction = "You are a helpful assistant for Q/A tasks. Respond to user's question in just Urdu language. Keep the answer short, concise and complete."
+model = genai.GenerativeModel('gemini-1.5-flash', generation_config={"temperature": 0.2, "max_output_tokens":200},
                               system_instruction=instruction)
 
 def text_to_speech(text, lang='ur'):
